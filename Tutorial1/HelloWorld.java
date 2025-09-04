fac1663c1ebef094cae0c07cf591fd72ccbac841
junpeng gai <34943011+jeo33@users.noreply.github.com>
2025-09-03 22:28:29 -0400

    Create HelloWorld.java

---
+++ Tutorial1/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Use System.out.println to print the string to standard output.
+        System.out.println("Hello, World!");
+    }
+}
+
+
