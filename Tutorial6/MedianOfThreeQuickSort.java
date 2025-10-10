1261cbebb678c1cf145811d54d1fcf89de6092b2
jeo33 <gjpwewq@hotmail.com>
2025-10-10 17:52:19 -0400

    Modify Median of 3

--- Tutorial6/MedianOfThreeQuickSort.java
+++ Tutorial6/MedianOfThreeQuickSort.java
@@ -1,64 +1,58 @@
 import java.util.Random;
-import java.util.Arrays;
 
 public class MedianOfThreeQuickSort {
     private static Random rand = new Random();
     
-    public static void quickSort(int[] arr, int left, int right) {
-        if (left < right) {
-            int pivotIndex = partition(arr, left, right);
-            
-            quickSort(arr, left, pivotIndex - 1);
-            quickSort(arr, pivotIndex + 1, right);
+    public static int median3Partition(int[] A, int p, int r) {
+        int mid = (p + r) / 2;
+        
+        int a = A[p];
+        int b = A[mid];
+        int c = A[r];
+        
+        int medianIndex;
+        
+        if ((a <= b && b <= c) || (c <= b && b <= a)) {
+            medianIndex = mid;
+        } else if ((b <= a && a <= c) || (c <= a && a <= b)) {
+            medianIndex = p;
+        } else {
+            medianIndex = r;
         }
-    }
-
-    public static int partition(int[] arr, int left, int right) {
-        // Find median of three: first, middle, last
-        int mid = left + (right - left) / 2;
         
-        // Get the median index
-        int medianIndex = getMedianOfThree(arr, left, mid, right);
+        int temp = A[r];
+        A[r] = A[medianIndex];
+        A[medianIndex] = temp;
         
-        // Swap median with last element (pivot position)
-        int temp = arr[medianIndex];
-        arr[medianIndex] = arr[right];
-        arr[right] = temp;
-        
-        // Standard partition with median as pivot
-        int pivot = arr[right];
-        int i = left - 1;
-        
-        for (int j = left; j < right; j++) {
-            if (arr[j] < pivot) {
-                i++;
-                temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-        }
-
-        temp = arr[i + 1];
-        arr[i + 1] = arr[right];
-        arr[right] = temp;
-        
-        return i + 1;
+        return partition(A, p, r);
     }
     
-    // Find index of median among three elements
-    public static int getMedianOfThree(int[] arr, int left, int mid, int right) {
-        int a = arr[left];
-        int b = arr[mid];
-        int c = arr[right];
-        
-        // Find median value and return its index
-        if ((a <= b && b <= c) || (c <= b && b <= a)) {
-            return mid;  // b is median
-        } else if ((b <= a && a <= c) || (c <= a && a <= b)) {
-            return left; // a is median
-        } else {
-            return right; // c is median
+    public static void median3QuickSort(int[] A, int p, int r) {
+        if (p < r) {
+            int q = median3Partition(A, p, r);
+            median3QuickSort(A, p, q - 1);
+            median3QuickSort(A, q + 1, r);
         }
+    }
+    
+    public static int partition(int[] A, int p, int r) {
+        int pivot = A[r];
+        int i = p - 1;
+        
+        for (int j = p; j < r; j++) {
+            if (A[j] <= pivot) {
+                i++;
+                int temp = A[i];
+                A[i] = A[j];
+                A[j] = temp;
+            }
+        }
+        
+        int temp = A[i + 1];
+        A[i + 1] = A[r];
+        A[r] = temp;
+        
+        return i + 1;
     }
     
     public static void main(String[] args) {
@@ -67,7 +61,7 @@ public class MedianOfThreeQuickSort {
         System.out.println("Original array:");
         printArray(arr);
         
-        quickSort(arr, 0, arr.length - 1);
+        median3QuickSort(arr, 0, arr.length - 1);
         
         System.out.println("Sorted array:");
         printArray(arr);
